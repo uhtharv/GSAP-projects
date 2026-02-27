fe6a47c8e67827c5a036c3245303e3d8cd46e1e8
uhtharv <atharv.webdev@outlook.com>
2026-02-27 15:10:58 +0530

    initial commit

--- pin page element scroll/script.js
+++ pin page element scroll/script.js
@@ -5,7 +5,27 @@ gsap.to("#page2 h1", {
         start: "top 0%",
         end: "top -150%",
         markers: false,
-        scrub:3,
-        pin:true
+        scrub: 2,
+        pin: true
     }
 })
+
+gsap.from("#page1 h1, img", {
+    y: -100,
+    opacity: 0,
+    duration: 1.3,
+    ease: "bounce.out",
+    stagger: 0.2
+})
+
+gsap.to("#page3 img", {
+    scrollTrigger: {
+        trigger: "#page3",
+        start: "top 40%"
+    },
+    duration: 1,
+    delay: 0.5,
+    rotation: 180,
+    ease: "power2.out"
+})
+
